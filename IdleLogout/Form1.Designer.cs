6c5e81f53b45fb9b35250edf01b559265207a68e
Jekyll <kalugin@outlook.com>
2021-03-17 16:31:59 +0300

    Add project files.

---
+++ IdleLogout/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace IdleLogout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox_enable = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_time = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.checkBox_forcedLogoff = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox_enable
+            // 
+            this.checkBox_enable.AutoSize = true;
+            this.checkBox_enable.Location = new System.Drawing.Point(12, 12);
+            this.checkBox_enable.Name = "checkBox_enable";
+            this.checkBox_enable.Size = new System.Drawing.Size(59, 17);
+            this.checkBox_enable.TabIndex = 0;
+            this.checkBox_enable.Text = "Enable";
+            this.checkBox_enable.UseVisualStyleBackColor = true;
+            this.checkBox_enable.CheckedChanged += new System.EventHandler(this.CheckBox_enable_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Idle time";
+            // 
+            // textBox_time
+            // 
+            this.textBox_time.Location = new System.Drawing.Point(61, 35);
+            this.textBox_time.Name = "textBox_time";
+            this.textBox_time.Size = new System.Drawing.Size(69, 20);
+            this.textBox_time.TabIndex = 2;
+            this.textBox_time.Leave += new System.EventHandler(this.TextBox_time_Leave);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.NotifyIcon1_DoubleClick);
+            // 
+            // checkBox_forcedLogoff
+            // 
+            this.checkBox_forcedLogoff.AutoSize = true;
+            this.checkBox_forcedLogoff.Location = new System.Drawing.Point(12, 61);
+            this.checkBox_forcedLogoff.Name = "checkBox_forcedLogoff";
+            this.checkBox_forcedLogoff.Size = new System.Drawing.Size(91, 17);
+            this.checkBox_forcedLogoff.TabIndex = 0;
+            this.checkBox_forcedLogoff.Text = "Forced log-off";
+            this.checkBox_forcedLogoff.UseVisualStyleBackColor = true;
+            this.checkBox_forcedLogoff.CheckedChanged += new System.EventHandler(this.CheckBox_forcedLogoff_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(144, 93);
+            this.Controls.Add(this.textBox_time);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox_forcedLogoff);
+            this.Controls.Add(this.checkBox_enable);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IdleMove";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox_enable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_time;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.CheckBox checkBox_forcedLogoff;
+    }
+}
+
---
+++ IdleLogout/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace IdleLogout
+{
+    public partial class Form1 : Form
+    {
+        private const string ConfigFileName = "config.ini";
+        private const string EnableConfigItem = "Enable";
+        private const string ForcedLogOffConfigItem = "ForcedLogOff";
+        private const string IdleDelayConfigItem = "IdleDelay";
+
+        private int _idleTime = 60;
+        private uint _lastIdleTime;
+        private bool _forcedLogOff;
+        private readonly Timer _systemTimer = new Timer();
+        private int _cursorMoveDelay;
+        private readonly Random _rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+            var autoEnable = false;
+            try
+            {
+                var configLines = File.ReadAllLines(ConfigFileName);
+                foreach (var line in configLines)
+                {
+                    var l = line.Split('=');
+                    if (l.Length < 2) continue;
+
+                    l[0] = l[0].Trim().ToUpper();
+                    if (l[0] == EnableConfigItem.ToUpper())
+                    {
+                        int.TryParse(l[1], out var value);
+                        autoEnable = value > 0;
+                    }
+                    else if (l[0] == ForcedLogOffConfigItem.ToUpper())
+                    {
+                        int.TryParse(l[1], out var value);
+                        _forcedLogOff = value > 0;
+                    }
+                    else if (l[0] == IdleDelayConfigItem.ToUpper())
+                    {
+                        int.TryParse(l[1], out _idleTime);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Can't load settings: " + exception);
+            }
+
+            textBox_time.Text = _idleTime.ToString();
+            _cursorMoveDelay = _rnd.Next(0, _idleTime);
+            _systemTimer.Enabled = false;
+            _systemTimer.Interval = 1000;
+            _systemTimer.Tick += TimerEvent;
+
+            checkBox_forcedLogoff.Checked = _forcedLogOff;
+            checkBox_enable.Checked = autoEnable;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (checkBox_enable.Checked)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var configText = "";
+            configText += EnableConfigItem + "=" + (_systemTimer.Enabled == true ? 1 : 0) + Environment.NewLine;
+            configText += ForcedLogOffConfigItem + "=" + (_forcedLogOff == true ? 1 : 0) + Environment.NewLine;
+            configText += IdleDelayConfigItem + "=" + _idleTime + Environment.NewLine;
+            try
+            {
+                File.WriteAllText(ConfigFileName, configText);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Can't save settings: " + exception);
+            }
+        }
+
+        private void NotifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void CheckBox_enable_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_enable.Checked)
+            {
+                // start TimerEvent
+                _systemTimer.Enabled = true;
+
+                // open form window
+                textBox_time.Enabled = false;
+            }
+            else
+            {
+                // stop timer
+                _systemTimer.Enabled = false;
+
+                // minimize form
+                textBox_time.Enabled = true;
+                notifyIcon1.Text = "Inactive";
+            }
+        }
+
+        private void CheckBox_forcedLogoff_CheckedChanged(object sender, EventArgs e)
+        {
+            _forcedLogOff = checkBox_forcedLogoff.Checked;
+        }
+
+        private void TextBox_time_Leave(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox_time.Text, out _idleTime)) textBox_time.Text = _idleTime.ToString();
+
+            _cursorMoveDelay = _idleTime;
+        }
+
+        private void TimerEvent(object sender, EventArgs args)
+        {
+            var currentIdleTime = GetIdleTime();
+
+            if (currentIdleTime < _lastIdleTime)
+            {
+                _cursorMoveDelay = _idleTime;
+                _lastIdleTime = currentIdleTime;
+            }
+
+            if (currentIdleTime - _lastIdleTime > _cursorMoveDelay * 1000)
+            {
+                WindowsLogOff();
+            }
+        }
+
+        private static uint GetIdleTime()
+        {
+            var lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)Marshal.SizeOf(lastInPut);
+            GetLastInputInfo(ref lastInPut);
+
+            return (uint)Environment.TickCount - lastInPut.dwTime;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LASTINPUTINFO
+        {
+            public static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
+
+            [MarshalAs(UnmanagedType.U4)] public uint cbSize;
+            [MarshalAs(UnmanagedType.U4)] public uint dwTime;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        public bool WindowsLogOff()
+        {
+            if (_forcedLogOff) return ExitWindowsEx(0 | 0x00000004, 0); // forced log-off
+            else return ExitWindowsEx(0, 0);
+        }
+    }
+}
